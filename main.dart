9f8cb065575f1ec97b4fcf5847c3a411812975e0
najeebG <158007079+najeebG@users.noreply.github.com>
2024-12-06 21:42:59 +0800

    app b fake app

--- main.dart
+++ main.dart
@@ -1,65 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppB());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  int _foregroundTime = 0;
-  DateTime? _startTime;
-  DateTime? _endTime;
-  bool _isAppInForeground = false;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      // App A is in the foreground, start the timer
-      setState(() {
-        _isAppInForeground = true;
-        _startTime = DateTime.now();
-      });
-    } else if (state == AppLifecycleState.paused) {
-      // App A is in the background, stop the timer
-      if (_startTime != null) {
-        _endTime = DateTime.now();
-        setState(() {
-          _foregroundTime += _endTime!.difference(_startTime!).inSeconds;
-        });
-      }
-      setState(() {
-        _isAppInForeground = false;
-      });
-    }
-  }
-
+class AppB extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('App A - Tracker')),
+        appBar: AppBar(title: Text('App B')),
         body: Center(
           child: Text(
-            _isAppInForeground
-                ? 'App A is in the foreground.\nTime: $_foregroundTime seconds'
-                : 'App A is in the background.\nTime: $_foregroundTime seconds',
+            'App B is running...',
             style: TextStyle(fontSize: 24),
-            textAlign: TextAlign.center,
           ),
         ),
       ),
